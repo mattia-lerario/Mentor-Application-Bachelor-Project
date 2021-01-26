db4baaa44c464b1aad0ea6cd3aeb788ec55629f9
toramagn <toramagn@gmail.com>
2021-01-26 12:56:06 +0100

    Classnames List.jsx
    Added classnames on Add User-button, Edit- and Delete-button in List.jsx.
    
    Reused the classes I used on the buttons in Update.jsx and Details.jsx.

--- Client/src/admin/users/List.jsx
+++ Client/src/admin/users/List.jsx
@@ -26,7 +26,10 @@ function List({ match }) {
         <div>
             <h1>Users</h1>
             <p>All users from secure (admin only) api end point:</p>
-            <Link to={`${path}/add`} className="btn btn-sm btn-success mb-2">Add User</Link>
+            <BtnWrapper>
+                <Link to={`${path}/add`} className="Btn MainBtn LinkBtn mb-2">Add User</Link>
+            </BtnWrapper>
+
             <table className="table table-striped">
                 <thead>
                     <tr>
