66ed1f53032a46de3bf46e1fea5ad7115533e3d2
toramagn <toramagn@gmail.com>
2021-02-24 15:07:25 +0100

    Cards
    Changed the design of the cards on the Dashboard, in dashoboardList.jsx.
    
    Added img, and made sections for metrics and contact-info in the bottom.

--- Client/src/dashboard/DashboardList.jsx
+++ Client/src/dashboard/DashboardList.jsx
@@ -6,9 +6,10 @@ import {ListWrapper} from '../style/styledcomponents';
 import { companyService } from '@/_services';
 import { mentorService } from '@/_services';
 
-//icon
+//icons
 import {HiOutlineMail} from 'react-icons/hi';
-
+import {AiFillPhone, AiOutlineNumber} from 'react-icons/ai';
+import {GrScheduleNew} from 'react-icons/gr';
 function DashboardList({ match }) {
     const { path } = match;
     const [company, setUsers] = useState(null);
@@ -25,13 +26,33 @@ function DashboardList({ match }) {
 
                 {company && company.map(company =>
 
-                <section className="card">
-                    <p className="companyName">{company.companyName}</p>
-                    <img className="companyImg">{company.companyImg}</img>
-                    <p>Sales Revenue: {company.salesRevenue}</p>
-                    <p>Company number: {company.companyNumber}</p>
-                    <p><HiOutlineMail/>: {company.email}</p>
-                </section>
+                <article className="card">
+                    <section className="cardImg">
+                        <img className="companyImg">{company.companyImg}</img> {/*Får ikke denne til å fungere. 
+                        Tanken var å kunne legge ved et bilde som respresenterer bedriften når man legger de til i databasen. -Tora.*/}
+                        
+                        {/*
+                        <p className="companyName">{company.companyName}</p>
+                        */}
+                    </section>
+
+                    <section className="cardMetric">
+                        <p>Sales Revenue: {company.salesRevenue}</p>
+                        <p>Company number: {company.companyNumber}</p>
+                    </section>
+
+                    <section className="cardBottom">
+                        <p><HiOutlineMail/></p> 
+                        {/*
+                        <p id="cardMail"><HiOutlineMail/>: {company.email}</p> 
+                        Evt bare ha mail-symbolet med en click funksjon for å sende mail til selskapet.
+                        Uten at hele mail-adressen står skrevet her. -Tora*/}
+                        <p><AiFillPhone/></p>
+                        <p><GrScheduleNew/></p>
+                        <p><AiOutlineNumber/>{company.phase}1</p> {/*Fungerer ikke, enda jeg har lagt det til i databasen også -Tora */}
+                        {/*Legge inn sortering etter phase-number? fra 1 til 5 el. -Tora */}
+                    </section>
+                </article>
                 )}              
         </ListWrapper>
         
