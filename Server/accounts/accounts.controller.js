2280d77644dcbcf3ae6bc779cb4596a8f76e6255
MattiaLerario <mattia@leratechsolutions.com>
2021-03-01 17:11:54 +0100

    Added Security on Accounts
    users can now update their own accoutns and admins everyone account

--- Server/accounts/accounts.controller.js
+++ Server/accounts/accounts.controller.js
@@ -159,9 +159,9 @@ function getAll(req, res, next) {
 
 function getById(req, res, next) {
     // users can get their own account and admins can get any account
-    /*if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
+    if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
         return res.status(401).json({ message: 'Unauthorized' });
-    }*/
+    }
 
     accountService.getById(req.params.id)
         .then(account => account ? res.json(account) : res.sendStatus(404))
@@ -208,9 +208,9 @@ function updateSchema(req, res, next) {
 
 function update(req, res, next) {
     // users can update their own account and admins can update any account
-    /*if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
+    if (req.params.id !== req.user.id && req.user.role !== Role.Admin) {
         return res.status(401).json({ message: 'Unauthorized' });
-    }*/
+    }
     
     accountService.update(req.params.id,req.body)
         .then(account => res.json(account))
