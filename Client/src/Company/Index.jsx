975effaf141a33e202c3851c4cbcf6b8b6ec87ec
MattiaLerario <mattia@leratechsolutions.com>
2021-03-18 11:47:49 +0100

    Revert "Changes the text in header, and tried to add an alert to onclick function"
    This reverts commit fc7fa9058c6456aa5c918c0a514161a88bbebc71.

--- Client/src/Company/Index.jsx
+++ Client/src/Company/Index.jsx
@@ -16,7 +16,8 @@ function Company({ match }) {
         <article className="p-4">
             <section className="container">
                 
-                <h1 className ="loggedInnAs" >Logged in as {user.firstName}</h1>
+                <h1>Hi {user.firstName}</h1>
+                <p>Information about your companies</p>
 
                 <Switch>
                     <Route exact path={path} component={CompanyList} />
