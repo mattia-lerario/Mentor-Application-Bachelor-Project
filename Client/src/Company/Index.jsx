b40eb276651572b23ce62ee52967ffb865904a48
MattiaLerario <mattia@leratechsolutions.com>
2021-03-18 12:16:11 +0100

    Update Index.jsx

--- Client/src/Company/Index.jsx
+++ Client/src/Company/Index.jsx
@@ -16,7 +16,6 @@ function Company({ match }) {
         <article className="p-4">
            
             <section className="container">
-
                 <h1>Hi {user.firstName}</h1>
                 <p>Information about your companies</p>
 
