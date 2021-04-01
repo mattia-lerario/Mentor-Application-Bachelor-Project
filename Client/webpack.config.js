335b7b9abcb74817f914f00bd7b1235312c944e7
MattiaLerario <mattia@leratechsolutions.com>
2021-04-01 13:57:10 +0200

    edited apiurl

--- Client/webpack.config.js
+++ Client/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
+            apiUrl: 'leratech.tech'
         })
     }
 }
